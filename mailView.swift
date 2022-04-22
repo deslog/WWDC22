d9fc256bf531355ee86c7006208ac01b1fcb4fd3
JI SOO <dlwltn815@gmail.com>
2022-04-22 13:44:50 +0900

    mailView 버튼 추가

--- mailView.swift
+++ mailView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct mailView: View {
+    @State var inbox = true
+    @State var unread = true
+    @State var junkAndTrash = true
+    @State var writemail = true
     
     let screenWidth = 390
     let screenHeight = 844
@@ -16,25 +20,116 @@ struct mailView: View {
         VStack {
             ZStack {
                 VStack {
-                    Text("step.2 Find three answer, too!🐨")
+                    Text("Find 4 answers!🐨")
                         .bold()
                         .font(.system(size: 30))
                     
                     Image("mailView")
                         .resizable()
-                    .frame(width: 342, height: 740)
+                        .frame(width: 342, height: 740)
                 }
-            }
+                
+                HStack {
+                    Button(action: {
+                        withAnimation(.easeIn) {
+                            inbox = false
+                        }
+                    }, label: {
+                        Text("999").bold()
+                            .fontWeight(.semibold)
+                            .font(.system(size: 15))
+                            .frame(width: 50, height: 35, alignment: .center)
+                            .foregroundColor(inbox ? Color.blue : Color.gray)
+                    })
+                    .padding()
+                    .frame(width: 40, height: 30, alignment: .center)
+                    //                    .background(inbox ? Color.red : Color.gray)
+                }
+                .offset(x: 98, y:-191)
+                
+                HStack {
+                    Button(action: {
+                        withAnimation(.easeIn) {
+                            unread = false
+                        }
+                    }, label: {
+                        Text("423").bold()
+                            .fontWeight(.semibold)
+                            .font(.system(size: 15))
+                            .frame(width: 50, height: 35, alignment: .center)
+                            .foregroundColor(unread ? Color.blue : Color.gray)
+                    })
+                    .padding()
+                    .frame(width: 40, height: 30, alignment: .center)
+                    //                    .background(inbox ? Color.red : Color.gray)
+                }
+                .offset(x: 98, y:-96)
+                
+                HStack {
+                    Button(action: {
+                        withAnimation(.easeIn) {
+                            junkAndTrash = false
+                        }
+                    }, label: {
+                        Text("23").bold()
+                            .fontWeight(.semibold)
+                            .font(.system(size: 15))
+                            .frame(width: 50, height: 35, alignment: .center)
+                            .foregroundColor(junkAndTrash ? Color.blue : Color.gray)
+                    })
+                    .padding()
+                    .frame(width: 40, height: 30, alignment: .center)
+                    //                    .background(inbox ? Color.red : Color.gray)
+                }
+                .offset(x: 98, y:-64)
+                
+                HStack {
+                    Button(action: {
+                        withAnimation(.easeIn) {
+                            junkAndTrash = false
+                        }
+                    }, label: {
+                        Text("498").bold()
+                            .fontWeight(.semibold)
+                            .font(.system(size: 15))
+                            .frame(width: 50, height: 35, alignment: .center)
+                            .foregroundColor(junkAndTrash ? Color.blue : Color.gray)
+                    })
+                    .padding()
+                    .frame(width: 40, height: 30, alignment: .center)
+                    //                    .background(inbox ? Color.red : Color.gray)
+                }
+                .offset(x: 98, y:-33)
+                
+                HStack {
+                    Button(action: {
+                        withAnimation(.easeIn) {
+                            writemail = false
+                        }
+                    }, label: {
+                        Image(systemName: "square.and.pencil")
+                            .foregroundColor(writemail ? Color.blue : Color.gray)
+                    })
+                    .padding()
+                    .frame(width: 40, height: 30, alignment: .center)
+                }
+                .offset(x: 120, y:330)
+                
+                ChatBubble(direction: .left) {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut semper quam. Phasellus non mauris sem. Donec sed fermentum eros. Donec pretium nec turpis a semper.")
+                        .font(.system(size: 20))
+                        .padding(.all, 20)
+                        .foregroundColor(Color.white)
+                        .background(Color.green)
+                        .frame(width: inbox ? 0 : 210)
+                }
+                .offset(x: 580, y: -350)
+                
+
+                
+            } // zstack 끝
             
-            ZStack {
-                // 말풍선 쌓기
-            }
-            
-            // next, hint 버튼 만들어야해
-            
-        } //hstack 끝
-        
-        
+        } //가장 상단 vstack 끝
         
     }
 }
