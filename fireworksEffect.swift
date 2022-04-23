bba733f079e72d5dbff47c7f37aa452100bd026b
JI SOO <dlwltn815@gmail.com>
2022-04-23 17:04:58 +0900

    fireworksEffect 불꽃놀이 파편 크기 수정

--- fireworksEffect.swift
+++ fireworksEffect.swift
@@ -43,18 +43,18 @@ struct ParticlesModifier: ViewModifier {
 
 struct fireworksEffectView: View {
     var body: some View {
-        ZStack {
-            Circle()
-                .fill(Color.blue)
-                .frame(width: 12, height: 12)
-                .modifier(ParticlesModifier())
-                .offset(x: -100, y : -50)
-            
-            Circle()
-                .fill(Color.red)
-                .frame(width: 12, height: 12)
-                .modifier(ParticlesModifier())
-                .offset(x: 60, y : 70)
+            ZStack {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 15, height: 15)
+                    .modifier(ParticlesModifier())
+                    .offset(x: -100, y : -50)
+                
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 15, height: 15)
+                    .modifier(ParticlesModifier())
+                    .offset(x: 60, y : 70)
         }
     }
 }
