6624dfb08848e3f4e9206c4bea504ef49f8e77ea
JI SOO <dlwltn815@gmail.com>
2022-04-23 22:36:33 +0900

    congratsView 글귀 수정

--- congratsView.swift
+++ congratsView.swift
@@ -12,27 +12,46 @@ struct congratsView: View {
 
     var body: some View {
         ZStack {
-            
-            fireworksEffectView()
-            
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                    .font(.system(size: 40))
+            Image("mainBackground")
+                .resizable()
+                .ignoresSafeArea()
                 
-                NavigationLink(destination: mainView()) {
-                    Text("Go to home") //케미가 한 것 처럼 binding 해야할 듯? false 주는거로,
-                        .font(.system(size: 30))
-                        .frame(width: 320, height: 70, alignment: .center)
-                        .background(Color.gray)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(30)
-                        .onTapGesture {
-                            firstNaviLinkActive = false
-                        }
+            VStack{
+                ZStack{
+                    ClappingHandsEmojiView()
+                    fireworksEffectView()
                 }
-                .navigationBarHidden(true)
+                .padding(.all, 30)
+                
+                VStack {
+                    
+                    Text("Thak you!")
+                        .bold()
+                        .font(.system(size: 40))
+                    
+                    Text(" Because of you, the earth will be healthy.\nFrom now on, let's work slowly and practice small things!\n ")
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Diwan Thuluth Regular", size: 25))
+                        .padding(EdgeInsets(top: 30, leading: 20, bottom: 10, trailing: 20))
+                        .foregroundColor(Color.white)
+                        .background(RoundedRectangle(cornerRadius: 30).fill(Color.black.opacity(0.5)))
+                        .padding(.all, 20)
+                    NavigationLink(destination: mainView()) {
+                        Text("Go to main")
+                            .font(.system(size: 30))
+                            .frame(width: 320, height: 70, alignment: .center)
+                            .background(Color.black)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(30)
+                            .onTapGesture {
+                                firstNaviLinkActive = false
+                            }
+                    }
+                    .navigationBarHidden(true)
+                }
             }
         }
+        
     }
 }
 
