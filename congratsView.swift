fce074bda64e569ed3c9581364b07139d5107363
JI SOO <dlwltn815@gmail.com>
2022-04-23 16:51:30 +0900

    fireworkEffect 기능 추가

--- congratsView.swift
+++ congratsView.swift
@@ -11,22 +11,27 @@ struct congratsView: View {
     @Binding var firstNaviLinkActive: Bool
 
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .font(.system(size: 40))
+        ZStack {
             
-            NavigationLink(destination: mainView()) {
-                Text("Go to home") //케미가 한 것 처럼 binding 해야할 듯? false 주는거로,
-                    .font(.system(size: 30))
-                    .frame(width: 320, height: 70, alignment: .center)
-                    .background(Color.gray)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(30)
-                    .onTapGesture {
-                        firstNaviLinkActive = false
-                    }
+            fireworksEffectView()
+            
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 40))
+                
+                NavigationLink(destination: mainView()) {
+                    Text("Go to home") //케미가 한 것 처럼 binding 해야할 듯? false 주는거로,
+                        .font(.system(size: 30))
+                        .frame(width: 320, height: 70, alignment: .center)
+                        .background(Color.gray)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(30)
+                        .onTapGesture {
+                            firstNaviLinkActive = false
+                        }
+                }
+                .navigationBarHidden(true)
             }
-            .navigationBarHidden(true)
         }
     }
 }
