61830666d68b707949bcab83fee03345712e7fe8
JI SOO <dlwltn815@gmail.com>
2022-04-24 16:51:15 +0900

    How To play gif 파일 생성, view 생성

---
+++ GifImage.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import WebKit
+
+struct GifImage: UIViewRepresentable{
+    private let name: String
+    init(_ name: String){
+        self.name = name
+    }
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        
+        print(name)
+        print("\n------------------------\n",Bundle.main,"\n------------------------\n")
+        let path = Bundle.main.path(forResource: name, ofType: "gif")
+        let url = URL(fileURLWithPath: path!)
+        let data = try! Data(contentsOf: url)
+        webView.load(
+            data,
+            mimeType: "image/gif",
+            characterEncodingName: "UTF-8",
+            baseURL: url.deletingLastPathComponent()
+        )
+        webView.scrollView.isScrollEnabled = false // 스크롤 막음. 기능
+        return webView
+    }
+    func updateUIView(_ uiView: WKWebView, context:Context){
+        uiView.reload()
+    }
+}
--- howToGameView.swift
+++ howToGameView.swift
@@ -10,8 +10,32 @@ import SwiftUI
 struct howToGameView: View {
     var body: some View {
         NavigationView {
-            Text("Hello, World!")
-                .font(.system(size: 30))
+            
+            VStack {
+                
+                GifImage("howToGame")
+                    .frame(width: 600, height: 600)
+                    .offset(x: -20, y: -50)
+                
+                Text(" How To Play 🎮 - This is example.")
+                    .font(.system(size: 30))
+                    .bold()
+                    .background(Color.blue.opacity(0.8))
+                    .foregroundColor(Color.white)
+                
+                Text("1️⃣ The number of answers is displayed as (0/3) in the title.\n2️⃣Touch the badge, app, number, function, etc. that can reduce CO2 in the iPhone frame.\n3️⃣Read the description.🤓\n4️⃣If you are not sure, click Hint.\n5️⃣When you have found all the correct answers, click Next.")
+                    .multilineTextAlignment(.leading)
+                    .font(.custom("Diwan Thuluth Regular", size: 22))
+                    .padding(EdgeInsets(top: 30, leading: 20, bottom: 10, trailing: 20))
+                    .foregroundColor(Color.white)
+                    .background(RoundedRectangle(cornerRadius: 30).fill(Color.black.opacity(0.5)))
+                    .padding(EdgeInsets(top: 10, leading: 30, bottom: 30, trailing: 30))
+                
+                Spacer()
+                
+                
+            }
+            
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
