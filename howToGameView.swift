0d7282cc76be8e0a37d7686121623d96cf59f721
JI SOO <dlwltn815@gmail.com>
2022-04-24 17:40:13 +0900

    how to play 화면 수정

--- howToGameView.swift
+++ howToGameView.swift
@@ -14,10 +14,19 @@ struct howToGameView: View {
         NavigationView {
             
             VStack {
+                ZStack {
+
+                    GifImage("howToGame")
+                        .frame(width: 600, height: 600)
+                        .offset(x: -20, y: -50)
+                    
+                    Text("Find Hidden CO2!🌳 (0/2)")
+                        .bold()
+                        .font(.system(size: 20))
+                        .foregroundColor(Color.black)
+                        .offset(x: 0, y: -350)
+                }
                 
-                GifImage("howToGame")
-                    .frame(width: 600, height: 600)
-                    .offset(x: -20, y: -50)
                 
                 Text(" How To Play 🎮 - This is example.")
                     .font(.system(size: 30))
@@ -25,7 +34,7 @@ struct howToGameView: View {
                     .background(Color.blue.opacity(0.8))
                     .foregroundColor(Color.white)
                 
-                Text("1️⃣ The number of answers is displayed as (0/3) in the title.\n2️⃣Touch the badge, app, number, function, etc. that can reduce CO2 in the iPhone frame.\n3️⃣Read the description.🤓\n4️⃣If you are not sure, click Hint.\n5️⃣When you have found all the correct answers, click Next.")
+                Text("1️⃣ The number of answers is displayed as (0/3) in the title.\n2️⃣Touch the badge, app, number, function, etc. that can reduce CO2 in the iPhone frame.\n3️⃣Read the green bubble chat.🤓\n4️⃣If you are not sure, click Hint.\n5️⃣When you have found all the correct answers, click Next.")
                     .multilineTextAlignment(.leading)
                     .font(.custom("Diwan Thuluth Regular", size: 22))
                     .padding(EdgeInsets(top: 30, leading: 20, bottom: 10, trailing: 20))
